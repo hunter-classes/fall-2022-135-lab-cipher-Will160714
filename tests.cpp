8967b61e779c39bb04ed50ccb02d2a1b733eda37
Will160714 <95445527+Will160714@users.noreply.github.com>
2022-11-03 14:20:13 -0400

    Added extra test

--- tests.cpp
+++ tests.cpp
@@ -8,6 +8,7 @@ TEST_CASE("Decryption"){
     CHECK(solve("tf zpzaly dhuaz av lha hwwslz. zhyh pz zdpttpun pu aol vjlhu.") == "my sister wants to eat apples. sara is swimming in the ocean.");
     CHECK(solve("ebiil, tloia. f xj x kbt moldoxjjbo fk mvqelk. texq ixkdrxdb lo ixkdrxdbp xob vlr molcfzfbkq fk?") == "hello, world. i am a new programmer in python. what language or languages are you proficient in?");
     CHECK(solve("what games do you play? i play minecraft and i hate the world, i am really creative. would you like to join me?") == "what games do you play? i play minecraft and i hate the world, i am really creative. would you like to join me?");
+    CHECK(solve("byffi, qilfx. nby nbcha ypyls jlialuggyl wixym qbyh nbys uly fyulhcha u hyq fuhaouay!") == "hello, world. the thing every programmer codes when they are learning a new language!");
     CHECK(solve(" ") == " ");
     CHECK(solve("!") == "!");
 }
