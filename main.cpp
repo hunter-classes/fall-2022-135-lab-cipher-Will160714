19ce6d2098a3e4b8809cbab833222c91d741b283
Will160714 <95445527+Will160714@users.noreply.github.com>
2022-11-03 14:20:27 -0400

    Showed a test case

--- main.cpp
+++ main.cpp
@@ -1,9 +1,13 @@
 #include <iostream>
-
-
+#include <string>
 #include "funcs.h"
 
 int main()
 {
+  std::string test = solve("byffi, qilfx. nby nbcha ypyls jlialuggyl wixym qbyh nbys uly fyulhcha u hyq fuhaouay");
+  std::cout << "Encrypted Message: " << std::endl;
+  std::cout << "byffi, qilfx. nby nbcha ypyls jlialuggyl wixym qbyh nbys uly fyulhcha u hyq fuhaouay" << std::endl;
+  std::cout << "Decrypted Message: " << std::endl;
+  std::cout << test << std::endl;
   return 0;
 }
